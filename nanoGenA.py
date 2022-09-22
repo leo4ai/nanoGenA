e337250d7c62dbaa6f9dccbb92f59046fb4408ec
Javier Nunez-Fontarnau <javier.nunez-fontarnau@helsinki.fi>
2022-09-22 14:03:46 +0300

    requirements and fast and slow versions

---
+++ nanoGenA.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import random
+import time
+
+    
+class Candidate():
+    
+    def __init__(self, chrom=None):
+
+        self.target = [c for c in "It was the best of times, it was the worst of times, " +
+                                    "it was the age of wisdom, it was the age of foolishness, " +
+                                    "it was the epoch of belief."]
+
+        self.alphabet = [chr(c) for c in range(ord(' '), ord('z')+1)]
+
+        self.alphabet_N = len(self.alphabet)
+        self.target_N = len(self.target)
+
+
+        
+        if (chrom == None):
+            self.chrom = [ self.alphabet[random.randint(0, self.alphabet_N-1)] for i in range(self.target_N) ]
+        else:
+            self.chrom = chrom
+            
+        self.fit = sum(1 for (a,b) in zip(self.chrom, self.target) if a != b)
+        
+        
+    def __lt__(self, other):
+        return self.fit < other.fit
+
+        
+    def __str__(self):
+        return "fit {fit}  {candidate}".format(fit = self.fit, candidate = ''.join([i for i in self.chrom ]))
+
+
+    def swap(self, a, b, prob):
+        return (b if random.random() < prob else a)
+
+
+    def crossover(self, other_candidate, prob):
+        new_chrom = [ self.swap(a, b, prob) for (a,b) in zip(self.chrom, other_candidate.chrom) ]
+        return Candidate(new_chrom)
+
+
+
+class Evolution():
+    
+    def __init__(self):
+        self.maxGen = 250*1000
+
+
+    def offspring(self, candidates, p_cross):
+        evo0 = [ candidates[i].crossover(candidates[i+2], p_cross) for i in range(3) ]
+        evo1 = [ candidates[i].crossover(Candidate(), p_cross * 2) for i in range(5) ]
+        
+        evo = [candidates[0]] + evo0 + evo1
+        evo.sort()
+        
+        return evo[:5]        
+    
+
+    def evolve(self):
+        candidates = ([ Candidate() for i in range(5) ])
+        candidates.sort()
+        
+        p_cross = 0.2
+        i = 0
+
+        while i < self.maxGen and candidates[0].fit > 0:
+            candidates = self.offspring(candidates, p_cross)            
+            if (i % 3000 == 0): 
+                print("generation {generation}  {candidate}".format(generation = i, candidate = candidates[0]))
+            p_cross = p_cross + 5e-7
+            i = i + 1
+        
+
+        return (i, candidates[0])
+    
+
+if __name__ == '__main__':
+
+    start = time.time()
+    (i, candidate) = Evolution().evolve()
+    elapsed = time.time() - start
+    print("\n" + str(i) + "  " + str(candidate))
+    print(str(elapsed) + " sec.")
---
+++ nanoGenA_parallel.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+
+import ray
+import numpy as np
+from numba import jit
+import time
+from itertools import chain
+
+
+targetStr = "It was the best of times, it was the worst of times, " \
+                "it was the age of wisdom, it was the age of foolishness, " \
+                "it was the epoch of belief."
+
+target = np.array([ord(c) for c in targetStr])
+targetLen = len(target)
+lower = ord(' ')
+upper = ord('z')
+    
+
+@jit(nopython=True)
+def fit(chrom):
+    return np.count_nonzero(chrom-target)
+
+
+@jit(nopython=True)
+def crossover(chrom1, chrom2, prob):
+    return \
+        np.array( [ (chrom2[i] if np.random.random() < prob else chrom1[i]) \
+            for i in np.arange(chrom1.size)] )
+    
+
+def toString(chrom):
+    return "fit {fit}  {candidate}" \
+                .format(fit = fit(chrom), candidate = ''.join(list(map(chr, chrom))))
+
+
+@jit(nopython=True)
+def randomChrom(n = targetLen):
+    return np.random.randint(lower, upper, size=n)
+
+
+@ray.remote
+def evolve(candidates, p_cross, max_iter):
+    i = 0
+    while i < max_iter and fit(candidates[0]) > 0:
+        evo0 = [ crossover(candidates[i], candidates[i+2], p_cross) for i in range(3) ]
+        evo = [candidates[0]] + evo0 + [ randomChrom() for _ in range(5)]
+        evo.sort(key=fit)
+        
+        candidates = evo[:5]
+        i = i + 1
+    
+    return candidates  
+
+
+
+def cycles(pool_N = 2):
+    
+    candidates = [ randomChrom() for i in range(5) ]
+    candidates.sort(key=fit)
+    p_cross = 0.1
+    i = 0
+    
+    while fit(candidates[0]) > 0:
+        max_iter = (1 + i/2) * 10
+        print("Cycle {cycle} (#{pop})  {candidate}" \
+                .format(cycle = i, pop = int(max_iter), candidate = toString(candidates[0])))
+        
+        subpopulations = ray.get( [evolve.remote(candidates, p_cross, max_iter) for _ in range(pool_N)] )
+        subpopulations = list(chain.from_iterable(subpopulations))
+        subpopulations.sort(key=fit)
+        
+        candidates = subpopulations[:5]
+        
+        p_cross = p_cross + 5e-7
+        i = i + 1
+        
+    return candidates[0]
+        
+    
+
+if __name__ == '__main__':
+
+    ray.init()
+    
+    start = time.time()
+    candidate = cycles(2)
+    elapsed = time.time() - start
+    
+    print("solution  " + toString(candidate))
+    print(str(elapsed) + " sec.")
